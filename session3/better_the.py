fda6ebfbbd3eb0461668cae64213ee5d0cf71615
gaf3 <gfitch@gaf3.com>
2014-09-26 19:23:52 -0400

    New examples

---
+++ session3/better_the.py
@@ -0,0 +1,8 @@
+def addto(thing,stuff=None):
+
+    if stuff is None:
+        stuff = []
+
+    stuff.append(thing)
+
+
---
+++ session3/moar_the.py
@@ -0,0 +1,12 @@
+def addto(ya=None,no=None,why=None):
+
+    if ya is not None:
+        print "Affirmative"
+
+    if no is not None:
+        print "Nu uh"
+
+    if why is None:
+        print "You should ask more questions"
+
+
